8b57e70fb690af8e5bbe2459bfb17647559db4aa
Niels Andriesse <andriesseniels@gmail.com>
2024-10-27 20:58:04 +0000

    Add donation link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FunctionComponent = () => {
     <div className='h-[96px] lg:h-[120px] flex flex-row items-center justify-between border-t border-t-white20 p-4 lg:p-8 gap-8'>
       <div className='flex flex-col gap-2'>
         <span className='text-xs'>CREATED BY NIELS ANDRIESSE</span>
-        <span className='text-xs'>SUPPORT THE APP BY <a href='/' target='_blank' className='underline'>DONATING</a></span>
+        <span className='text-xs'>SUPPORT THE APP BY <a href='https://buymeacoffee.com/rocketindex' target='_blank' className='underline'>DONATING</a></span>
         <span className='text-xs'>CONTRIBUTE ON <a href='https://github.com/nielsandriesse/rocket-index' target='_blank' className='underline'>GITHUB</a></span>
       </div>
     </div>
